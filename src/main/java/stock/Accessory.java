7c0fbfd3d4c96997019f4fa924e5b32e2c5e98b9
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-10 11:41:47 +0000

    Added getters and TDD tests for Accessory class.

--- src/main/java/stock/Accessory.java
+++ src/main/java/stock/Accessory.java
@@ -1,4 +1,34 @@
 package stock;
 
+import enums.AccessoryType;
+
 public class Accessory {
+
+    private String description;
+    private AccessoryType accessoryType;
+    private double wholeSalePrice;
+    private double retailPrice;
+
+    public Accessory(String description, AccessoryType accessoryType, double wholeSalePrice) {
+        this.description = description;
+        this.accessoryType = accessoryType;
+        this.wholeSalePrice = wholeSalePrice;
+        this.retailPrice = 0;                    // Initialize to zero/null until we have calculated the markup
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public AccessoryType getAccessoryType() {
+        return accessoryType;
+    }
+
+    public double getWholeSalePrice() {
+        return wholeSalePrice;
+    }
+
+    public double getRetailPrice() {
+        return retailPrice;
+    }
 }
--- src/test/java/stock/TestAccessory.java
+++ src/test/java/stock/TestAccessory.java
@@ -1,11 +1,32 @@
 package stock;
 
 import enums.AccessoryType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestAccessory {
 
-    private String description;
-    private AccessoryType accessoryType;
-    private double wholeSalePrice;
-    private double retailPrice;
+    Accessory accessory;
+
+    @Before
+    public void setUp() throws Exception {
+        accessory = new Accessory("Long-handled drum stick", AccessoryType.DRUM_STICK, 22.50);
+    }
+
+    @Test
+    public void getDescription() {
+        assertEquals("Long-handled drum stick", accessory.getDescription());
+    }
+
+    @Test
+    public void getAccessoryType() {
+        assertEquals(AccessoryType.DRUM_STICK, accessory.getAccessoryType());
+    }
+
+    @Test
+    public void getWholeSalePrice() {
+        assertEquals(22.50, accessory.getWholeSalePrice(), 0.01);
+    }
 }
