1d87906cffe830c375084c9112e0e5fa4da658b6
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-10 13:40:47 +0000

    Added wholeSalePrice and retailPrice properties to Instrument class.

--- src/main/java/stock/Instrument.java
+++ src/main/java/stock/Instrument.java
@@ -8,12 +8,16 @@ public class Instrument {
     private InstrumentMake make;
     private InstrumentMaterial material;
     private InstrumentType type;
+    private double wholeSalePrice;
+    private double retailPrice;
 
-    public Instrument(InstrumentColour colour, InstrumentMake make, InstrumentMaterial material, InstrumentType type) {
+    public Instrument(InstrumentColour colour, InstrumentMake make, InstrumentMaterial material, InstrumentType type, double wholeSalePrice) {
         this.colour = colour;
         this.make = make;
         this.material = material;
         this.type = type;
+        this.wholeSalePrice = wholeSalePrice;
+        this.retailPrice = 0;                    // Initialize to zero/null until we have calculated the markup
     }
 
     public InstrumentColour getColour() {
